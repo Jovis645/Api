454526a7b4d40cf18d2bc6e0ffc56f05a7a71d40
Johanna <jovis828@gmail.com>
2025-04-27 18:30:42 -0500

    Aprendiendo Api

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php 
+
+    // // Permitir CORS (para que otra app pueda consumir la API desde otro dominio o carpeta)
+    // header("Access-Control-Allow-Origin: *");
+    // header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    // header("Access-Control-Allow-Headers: Content-Type");
+
+
+    $host= "localhost";
+    $usuario="root";
+    $password="";
+    $basededatos="barberia";
+
+    $conexion= new mysqli($host,$usuario,$password,$basededatos);
+
+    if($conexion->connect_error){
+        die("Conexión no establecida". $conexion->connect_error);
+    }
+    
+    // Devolver en un archivo json
+    header("Content-Type: application/json"); 
+
+    // variable server, contiene la info que se recibe
+    $metodo= $_SERVER['REQUEST_METHOD'];
+
+    // // imprimir el metodo (Get,Put,Post,Delete...) que está llegando
+    // print_r($metodo);
+
+    // ruta para obtener la info del id, del dato que buscamos 
+    $path= isset($_SERVER['PATH_INFO'])?$_SERVER['PATH_INFO']:'/';
+
+    // El id que busco está dentro de la url
+    $buscarId= explode('/', $path);
+
+    $id=($path!=='/') ? end($buscarId):null;
+
+
+
+    // validar el método en una serie de casos, ejemplo switch. Validando las solicitudes
+    switch($metodo){
+
+        // SELECT, realiza una consulta, clientes
+        case 'GET':
+            consulta($conexion, $id);
+            break;
+        // INSERT, el POST es para insertar datos
+        case 'POST':
+            insertar($conexion);
+            break;
+         // UPDATE, para la actualización
+         case 'PUT':
+            actualizar($conexion, $id);
+            break;
+         // DELETE, borrar los registros
+         case 'DELETE':
+            borrar($conexion, $id);
+            break;
+        default:
+            echo "Método no permitido";
+            break;
+    }
+
+
+    // GET
+    function consulta($conexion, $id){
+        $sql= ($id===null) ? "SELECT * FROM cliente": "SELECT * FROM cliente WHERE idCliente=$id";
+
+        // Mostrar la información
+        $resultado= $conexion->query($sql);
+
+        if($resultado){
+            //El array almacena la info para mostrarla después
+           $datos= array();
+           while($fila= $resultado->fetch_assoc()){
+                //Almacenar los datos cuando vengan de la fila
+                $datos[]= $fila;
+           }
+           //Convertir toda la info de la base de datos en formato json
+           echo json_encode($datos);
+
+        }
+
+    }
+
+    //POST
+    function insertar($conexion){
+
+        // Decodificar el json, a partir de un envio de datos y
+        // obtener toda la info con file_get_contents
+        $dato= json_decode(file_get_contents('php://input'),true);
+        $nombre = $dato['nombre'];
+        $apellidos = $dato['apellidos'];
+        $telefono = $dato['telefono'];
+        $correo = $dato['correo'];
+        // print_r($nombre);
+
+        
+        $sql= "INSERT INTO cliente(nombre, apellidos, telefono, correo) VALUES ('$nombre', '$apellidos', '$telefono', '$correo')";
+        $resultado= $conexion->query($sql);
+
+        if($resultado){
+            $dato['id']= $conexion->insert_id;
+            echo json_encode($dato);
+        }else{
+            echo json_encode(array('error' => 'Error al crear cliente', 'detalle' => $conexion->error));
+        }
+
+    }
+
+    //DELETE
+    function borrar($conexion, $id){
+
+        echo "El id a borrar es: ". $id; 
+
+        $sql= "DELETE FROM cliente WHERE idCliente = $id";
+        $resultado= $conexion->query($sql);
+
+        if($resultado){
+            echo json_encode(array('mensaje' => 'Cliente borrado'));
+        }else{
+            echo json_encode(array('error' => 'Error al borrar cliente', 'detalle' => $conexion->error));
+        }
+
+    }
+
+    //UPDATE
+    function actualizar($conexion, $id){
+
+        $dato= json_decode(file_get_contents('php://input'),true);
+        $nombre = $dato['nombre'];
+
+        echo "El id a editar es: ". $id. " con el dato ". $nombre;
+
+        $sql= "UPDATE cliente SET nombre = '$nombre' WHERE idCliente = $id";
+        $resultado= $conexion->query($sql);
+
+        if($resultado){
+            echo json_encode(array('mensaje' => 'Cliente actualizado'));
+        }else{
+            echo json_encode(array('error' => 'Error al actualizar el cliente', 'detalle' => $conexion->error));
+        }
+
+    }
+
+?>
